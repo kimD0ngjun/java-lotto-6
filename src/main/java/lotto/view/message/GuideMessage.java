398d6ce785f8c998a007242e2ef5135d490ca673
kimD0ngjun <chickentasty0112@gmail.com>
2023-12-04 23:09:44 +0900

    [feat] view 입력 메시지 열거형 작성

--- src/main/java/lotto/view/message/GuideMessage.java
+++ src/main/java/lotto/view/message/GuideMessage.java
@@ -1,13 +1,12 @@
 package lotto.view.message;
 
-public enum InputMessage {
+public enum GuideMessage {
     PURCHASE("구입금액을 입력해 주세요."),
-    WINNING("당첨 번호를 입력해 주세요."),
-    BONUS("보너스 번호를 입력해 주세요.");
+    INPUT_MESSAGE("구입금액을 입력해 주세요.");
 
     private final String message;
 
-    InputMessage(String message) {
+    GuideMessage(String message) {
         this.message = message;
     }
 
