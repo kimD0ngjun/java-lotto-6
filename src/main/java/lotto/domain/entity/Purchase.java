80c1909c1986c859cea781c9d83d96f49e8bf44f
kimD0ngjun <chickentasty0112@gmail.com>
2023-12-04 23:49:15 +0900

    [refactoring] 로또 번호 개수 단위 생성 클래스 서비스 분리

--- src/main/java/lotto/domain/entity/Purchase.java
+++ src/main/java/lotto/domain/entity/Purchase.java
@@ -6,41 +6,24 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import lotto.service.LottoGenerator;
 
 public class Purchase {
     private int amount;
+    private final LottoGenerator lottoGenerator;
 
-    public Purchase(int amount) {
+    public Purchase(int amount, LottoGenerator lottoGenerator) {
         this.amount = amount / 1000;
+        this.lottoGenerator = lottoGenerator;
     }
 
     // 구입 금액만큼 생성 반복하기
     public List<List<Integer>> repeatGenerator() {
         List<List<Integer>> generatedNumbers = new ArrayList<>();
         while (amount > 0) {
-            generatedNumbers.add(generateNumbers());
+            generatedNumbers.add(lottoGenerator.generateNumbers());
             amount--;
         }
         return generatedNumbers;
-    }
-
-    // 로또 번호 생성기
-    private List<Integer> generateNumbers() {
-        List<Integer> numbers;
-        do {
-            numbers = new ArrayList<>(Randoms.pickUniqueNumbersInRange(1, 45, 6));
-        } while (hasDuplicates(numbers));
-        Collections.sort(numbers);
-        return Collections.unmodifiableList(numbers);
-    }
-
-    private boolean hasDuplicates(List<Integer> numbers) {
-        Set<Integer> set = new HashSet<>();
-        for (int num : numbers) {
-            if (!set.add(num)) {
-                return true;
-            }
-        }
-        return false;
     }
 }
