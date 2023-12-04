ed4e1398067171b2c1e08bc7a6b5f4f60ee0d27a
kimD0ngjun <chickentasty0112@gmail.com>
2023-12-05 01:02:08 +0900

    [refactoring] utility 입력값 타입 변환 로직 수정

--- src/main/java/lotto/utility/NumbersValidator.java
+++ src/main/java/lotto/utility/NumbersValidator.java
@@ -1,11 +1,12 @@
 package lotto.utility;
 
 import java.util.List;
+import lotto.view.message.ErrorMessage;
 
 public class NumbersValidator {
     public static void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(ErrorMessage.SIX_TIMES.getMessage());
         }
     }
 }
--- src/main/java/lotto/utility/TypeChanger.java
+++ src/main/java/lotto/utility/TypeChanger.java
@@ -3,14 +3,20 @@ package lotto.utility;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import lotto.view.message.ErrorMessage;
 
 public class TypeChanger {
 
     // 쉼표를 구분자로 List 반환하는 메소드
-    public static List<String> splitByComma(String input) {
-        return Arrays.stream(input.split(","))
-                .map(String::trim)
-                .collect(Collectors.toList());
+    public static List<Integer> splitAndParseInt(String input) {
+        try {
+            return Arrays.stream(input.split(","))
+                    .map(String::trim)
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ErrorMessage.ONLY_NUMBER.getMessage());
+        }
     }
 
     // 세 자리마다 숫자에 쉼표를 붙이는 메소드
