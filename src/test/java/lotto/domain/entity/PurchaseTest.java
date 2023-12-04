8781a12e82d044234a5f84417a38c798118e1354
kimD0ngjun <chickentasty0112@gmail.com>
2023-12-05 00:14:45 +0900

    [test] 구입 로또 번호 테스트 코드 상속으로 mock 구현 및 작성

---
+++ src/test/java/lotto/domain/entity/PurchaseTest.java
@@ -0,0 +1,62 @@
+package lotto.domain.entity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+import java.util.Arrays;
+import java.util.List;
+import lotto.service.PurchaseNumbersGenerator;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class PurchaseTest {
+    // 테스트용 Mock PurchaseNumbersGenerator 클래스
+    private static class MockPurchaseNumbersGenerator extends PurchaseNumbersGenerator {
+        @Override
+        public List<Integer> generateNumbers() {
+            return Arrays.asList(1, 2, 3, 4, 5, 6);
+        }
+    }
+
+    private static List<List<Integer>> generatedNumbers;
+
+    @BeforeAll
+    static void setUp() {
+        PurchaseNumbersGenerator mockGenerator = new MockPurchaseNumbersGenerator(); // 자동 타입 변환
+        Purchase purchase = new SimplePurchase(10000, mockGenerator); // 자동 타입 변환
+
+        generatedNumbers = purchase.repeatGenerator();
+    }
+
+    @DisplayName("로또 번호 생성기는 입력 금액 / 1000 만큼의 개수가 출력된다.")
+    @Test
+    void testRepeatGenerator() {
+        assertThat(generatedNumbers).hasSize(10);
+    }
+
+    @DisplayName("로또 번호 생성기의 각 단위 로또 번호의 길이는 6이다.")
+    @Test
+    void testUnitNumbersLength() {
+        for (List<Integer> numbers : generatedNumbers) {
+            assertThat(numbers).hasSize(6);
+        }
+    }
+
+    @DisplayName("로또 번호 생성기에서 반환된 리스트의 각 요소들이 중복되지 않는다.")
+    @Test
+    void testUnitNumbersDuplicates() {
+        for (List<Integer> numbers : generatedNumbers) {
+            assertThatCode(() -> assertThat(numbers).doesNotHaveDuplicates())
+                    .doesNotThrowAnyException();
+        }
+    }
+
+    @DisplayName("로또 번호 생성기에서 반환된 리스트는 오름차순으로 나열된다.")
+    @Test
+    void testUnitNumbersAscending() {
+        for (List<Integer> numbers : generatedNumbers) {
+            assertThat(numbers).isSorted();
+        }
+    }
+}
