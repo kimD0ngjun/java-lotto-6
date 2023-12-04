b9a14e6e0896f56f620aa8ea24b30060aefb55d9
kimD0ngjun <chickentasty0112@gmail.com>
2023-12-05 00:21:59 +0900

    [feat] utility 구입 관련 예외 검증 추가 구현

--- src/main/java/lotto/utility/PurchaseValidator.java
+++ src/main/java/lotto/utility/PurchaseValidator.java
@@ -3,11 +3,23 @@ package lotto.utility;
 import lotto.view.message.ErrorMessage;
 
 public class PurchaseValidator {
-    public static void isNumberType(String input) {
+    public static void isValidPurchase(String input) {
+        isNumberType(input);
+        int amount = Integer.parseInt(input);
+        isThousandsUnit(amount);
+    }
+
+    private static void isNumberType(String input) {
         try {
             Integer.parseInt(input);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(ErrorMessage.ONLY_NUMBER.getMessage());
+        }
+    }
+
+    private static void isThousandsUnit(int input) {
+        if (input % 1_000 != 0) {
+            throw new IllegalArgumentException(ErrorMessage.UNIT_VIOLATION.getMessage());
         }
     }
 }
