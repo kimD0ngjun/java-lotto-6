601f603a661b6aef4dc9f819cc91af2b9a529992
kimD0ngjun <chickentasty0112@gmail.com>
2023-12-04 23:41:34 +0900

    [feat] domain 로또 번호 생성 기능 메소드 작성

--- src/main/java/lotto/domain/entity/Purchase.java
+++ src/main/java/lotto/domain/entity/Purchase.java
@@ -1,12 +1,38 @@
 package lotto.domain.entity;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Purchase {
-    private final int amount;
+    private int amount;
 
     public Purchase(int amount) {
-        this.amount = amount;
+        this.amount = amount / 1000;
     }
 
-    // 필요 메소드 구현
 
+
+    // 로또 번호 생성기
+    private List<Integer> generateNumbers() {
+        List<Integer> numbers;
+        do {
+            numbers = new ArrayList<>(Randoms.pickUniqueNumbersInRange(1, 45, 6));
+        } while (hasDuplicates(numbers));
+        Collections.sort(numbers);
+        return Collections.unmodifiableList(numbers);
+    }
+
+    private boolean hasDuplicates(List<Integer> numbers) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : numbers) {
+            if (!set.add(num)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
